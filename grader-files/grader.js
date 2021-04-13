71efb2704d7ef4f6f56fa5816c8857f2c2708651
Pantek59 <j.frece@pantek.org>
2021-04-13 13:35:37 +0200

    Moved JS files to sub-directory, added group filter

--- grader-files/grader.js
+++ grader-files/grader.js
@@ -1,6 +1,7 @@
 const color_palette = ["#0087ff", "#00d75f", "#8787ff", "#afaf00", "#d7ff00", "#ff5faf"];
 let grader_configuration;
 let grader_class;
+let grader_groups = [];
 
 function read_config(){
     let file = document.getElementById("config_file").files[0];
@@ -19,11 +20,31 @@ function read_class(){
         let class_raw = reader.result;
         document.getElementById("class_area").value = class_raw;
         grader_class = CSVToArray(class_raw,";");
+        for (let student of grader_class){
+            grader_groups.push(student[1]);
+        }
+        grader_groups = grader_groups.filter(function (value, index, self) {
+            return self.indexOf(value) === index
+        });
+        let group_string = "";
+        $("#group_selector").empty();
+        for (let g of grader_groups){
+            $("#group_selector").append("<option>"+ g +"</option>")
+        }
     });
     reader.readAsText(file);
 }
 
 function init_grading(){
+    let active_group = [];
+    for (let s of grader_class){
+        console.log(s[1]);
+        console.log($("#group_selector option:selected").text());
+        if (s[1] === $("#group_selector option:selected").text()){
+            active_group.push(s);
+        }
+    }
+    console.log(active_group);
     console.log(grader_configuration);
     $("#startup_box").hide();
     let dimensions = Object.keys(grader_configuration);
@@ -37,7 +58,7 @@ function init_grading(){
 
     // Create headers
     let grid_string = "<div class=\"row\"><div class=\"col\"> </div>";
-    for (let student of grader_class){
+    for (let student of active_group){
         grid_string += "<div class=\"col\">"+ student[0] +"</div>"
     }
     grid_string += "</div>";
@@ -52,7 +73,7 @@ function init_grading(){
             if (asp === "[unnamed]" || grader_configuration[dim].aspect === asp){
                 grid_string += "<div class=\"row mb-2 align-items-center \" style='background-color: "+ color_palette[color_counter] +"'>";
                 grid_string += "<div class=\"col align-self-center\">"+ dim.replaceAll("_", " ") + "</div>";
-                for (let student of grader_class){
+                for (let student of active_group){
                     grid_string += "<div class=\"col align-self-center\"><input type='number' min='"+ grader_configuration[dim].min +"' max='"+ grader_configuration[dim].max +"' style=\"width: 50px;\"></div>"
                 }
                 grid_string += "</div>";
